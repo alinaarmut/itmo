de718ed85835b79eb9e2dc0ff2b22c9ba008074d
Alina <114672812+alinaarmut@users.noreply.github.com>
2023-09-13 18:09:55 +0300

    Delete прога/6_лаба directory

--- прога/6_лаба/Server/src/main/java/itmo/ser/commands/exceptions/AddressServerException.java
+++
@@ -1,7 +0,0 @@
-package itmo.ser.commands.exceptions;
-
-public class AddressServerException extends Exception{
-    public AddressServerException(String message) {
-        super(message);
-    }
-}
